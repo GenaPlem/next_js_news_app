2db1dfa1f848d3389204e44bf3db7e5f63e0ff76
GenaPlem <shainkill1337@gmail.com>
2024-10-28 15:57:07 +0000

    Update NewsPage with dummy news data

--- app/news/page.js
+++ app/news/page.js
@@ -1,19 +1,23 @@
 import Link from "next/link";
 
+import { DUMMY_NEWS } from "@/dummy-news";
+
 export default function NewsPage() {
   return (
     <>
       <h1>Fresh News every second</h1>
       <ul className="news-list">
-        <li>
-          <Link href="/news/concert">Concert</Link>
-        </li>
-        <li>
-          <Link href="/news/storm">Upcoming Storm</Link>
-        </li>
-        <li>
-          <Link href="/news/big_sales">Big Sales</Link>
-        </li>
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
