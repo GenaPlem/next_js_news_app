15d0a5c5bab5b8de42d84cfcd7cc0e1097ca4684
GenaPlem <shainkill1337@gmail.com>
2024-10-28 16:50:26 +0000

    Add default page for latest news parallel route

--- app/archive/@archive/[year]/page.js
+++ app/archive/@archive/[year]/page.js
@@ -4,5 +4,6 @@ import { getNewsForYear } from "@/lib/news";
 export default function FilteredNewsPage({ params }) {
   const newsYear = params.year;
   const news = getNewsForYear(newsYear);
+
   return <NewsList news={news} />;
 }
