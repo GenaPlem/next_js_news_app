7077e953d6a7d1314dc62cb528501bf4cbeed8d6
GenaPlem <shainkill1337@gmail.com>
2024-10-28 15:09:57 +0000

    Add News page

---
+++ app/news/page.js
@@ -0,0 +1,12 @@
+export default function News() {
+  return (
+    <>
+      <h1>Fresh News every second</h1>
+      <ul>
+        <li>Links</li>
+        <li>Links</li>
+        <li>Links</li>
+      </ul>
+    </>
+  );
+}
