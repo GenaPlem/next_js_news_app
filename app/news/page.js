086d352c6854d6cdfacfe0250d9dea3f9e38575f
GenaPlem <shainkill1337@gmail.com>
2024-10-28 15:19:33 +0000

    Add Links to News page

--- app/news/page.js
+++ app/news/page.js
@@ -1,11 +1,25 @@
+import Link from "next/link";
+
 export default function News() {
   return (
     <>
       <h1>Fresh News every second</h1>
       <ul>
-        <li>Links</li>
-        <li>Links</li>
-        <li>Links</li>
+        <li>
+          <p>
+            <Link href="/news/concert">Concert</Link>
+          </p>
+        </li>
+        <li>
+          <p>
+            <Link href="/news/storm">Upcoming Storm</Link>
+          </p>
+        </li>
+        <li>
+          <p>
+            <Link href="/news/big_sales">Big Sales</Link>
+          </p>
+        </li>
       </ul>
     </>
   );
